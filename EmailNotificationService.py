af16628aaf8411d03347aba5f67e0c4bb5fee16c
LiaShu <liashu2023@gmail.com>
2023-05-30 09:59:24 +0300

    version 1.6

--- EmailNotificationService.py
+++ EmailNotificationService.py
@@ -1,8 +1,10 @@
 import smtplib
+from email.mime.image import MIMEImage
 from string import Template
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from app_tasks import app_tasks
+
 
 class EmailService():
 
@@ -24,7 +26,65 @@ class EmailService():
             msg['To'] = email
             msg['Subject'] = "This is TEST"
             message = f'Dear user! This is your task today: {task}. Regards'
-            msg.attach(MIMEText(message, 'plain'))
+            #msg.attach(MIMEText(message, 'plain'))
+            html = f"""<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>НЛМК IIoT</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body style="margin: 0; padding: 0;">
+<table align="center" bgcolor="#c1c6c8" border="0" cellpadding="0" cellspacing="0" width="600">
+    <tr>
+        <td align="right" background="cid:image1" height="30px" style="padding: 40px 50px 30px 0;">
+
+            <font face="Arial" color="#2c5697" size="4"><b>Fly Lady</b></font>
+            <img src="cid:logo" width = "50%">
+        </td>
+    </tr>
+    <tr>
+        <td bgcolor="#ffffff" style="padding: 40px 30px 40px 30px;">
+            <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                <tr>
+                    <td>
+                        <font face="Arial" color="#0092bc"><b>ЦХПП.НТА-1</b></font>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="padding: 20px 0 30px 0;">
+                        <font face="Arial"><b> Внимание! ${message}!</b><br></font>
+                        <br>
+                        Теги: ***, ***, ***<br>
+                        Время: 31.03.2021 09:00 - 31.03.2021 09:10<br>
+                        <br>
+                        Отклонение от заданного значения более 15%
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        URL куда-нибудь
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+    <tr>
+        <td bgcolor="#ee4c50">
+            Row 3
+        </td>
+    </tr>
+</table>
+</body>
+</html>
+            """
+            msg.attach(MIMEText(html, 'html', 'utf-8'))
+            fp = open('header_logo.jpg', 'rb')
+            msg_image = MIMEImage(fp.read())
+            fp.close()
+            # Define the image's ID as referenced above
+            msg_image.add_header('Content-ID', '<image1>')
+            msg.attach(msg_image)
             s.send_message(msg)
             del msg
         s.quit()
--- test.py
+++ test.py
@@ -1,4 +1,34 @@
+# -*- coding: utf-8 -*-
+data = [('flyletter23@gmail.com', 'Кухонные полотенца', 'Постирайте в машинке.'),
+        ('flyletter23@gmail.com', 'Посуда', 'Перед сном вымойте.'),
+        ('liashu2023@gmail.com', 'Кухонный стол', 'Протирайте стол.'),
+        ('natalya9448@yandex.ru', 'Кухонный стол', 'Протирайте стол.'),
+        ('liashu2023@gmail.com', 'Миски домашних питомцев', 'Большинство мисок можно мыть в посудомоечной машине. '),
+        ('natalya9448@yandex.ru', 'Кухонные шкафчики', 'Протрите тряпкой с чистящим средством. '),
+        ('flyletter23@gmail.com', 'Кухонные шкафчики', 'Протрите тряпкой с чистящим средством. ')]
 
-my_list = ["apple", "banana", "cherry"]
-result = ", ".join(my_list)
-print(result)
+dict_task = {}
+final_list = []
+for i in data:
+     list_i = list(i)
+     final_list.append(list_i)
+for j in final_list:
+     email = j[0]
+     value = j[1::]
+     if email not in dict_task.keys():
+          dict_task[email] = value
+     else:
+          new_value = j[1::]
+          f_val = dict_task.get(email) + new_value
+          dict_task[email] = f_val
+print(dict_task)
+for k, v in dict_task.items():
+    email = k
+    task = v
+
+#      list_test.append(i[0])
+#      list_test.append(list(i[1:]))
+#      final_list.append(list_test)
+# list_name = list_test[::2]
+# list_task = list_test[1::2]
+# print(final_list)
